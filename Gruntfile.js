20a32e435253564b7b4ee252a700cc356558aab7
Jan-Henrik <jh.bruhn@me.com>
2014-05-15 20:30:13 +0200

    Implemented dist for linux, improved some more stuff

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,11 @@
-var wrench      = require('wrench'),
-    http        = require('http'),
-    url         = require('url'),
-    fs          = require('fs'),
-    ProgressBar = require('progress'),
-    DecompressZip = require('decompress-zip');
+var wrench        = require('wrench'),
+    http          = require('http'),
+    url           = require('url'),
+    fs            = require('fs'),
+    ProgressBar   = require('progress'),
+    DecompressZip = require('decompress-zip'),
+    tar           = require('tar-fs'),
+    zlib          = require('zlib');
 
 var nodeWebkitVersion = "0.9.2";
 
@@ -13,7 +15,7 @@ var nwFormats = {
   "linux": "tar.gz"
 }
 
-var distFiles = ["build/**", './node_modules/**', '!./node_modules/grunt*/**',
+var distFiles = ["build/**/*.*", './node_modules/**', '!./node_modules/grunt*/**',
              '!./node_modules/nodewebkit*/**', "package.json", 'index.html'];
 
 var downloadFile = function(targetFile, remoteUrl, done) {
@@ -59,7 +61,7 @@ function getLocalNodeWebkitDownloadPath(os, arch, format) {
 }
 
 module.exports = function(grunt) {
-
+  require('time-grunt')(grunt);
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
@@ -89,6 +91,7 @@ module.exports = function(grunt) {
     if(fs.existsSync(targetFile)) {
       grunt.log.writeln('skip download: file exists: ' + targetFile);
       done();
+      return;
     }
 
     grunt.log.writeln('Starting download.');
@@ -105,11 +108,11 @@ module.exports = function(grunt) {
     var done = this.async();
 
     var format = nwFormats[os];
+    var targetPath = 'dist/' + os + arch;
+    var filename = getLocalNodeWebkitDownloadPath(os, arch, format);
 
     if(format == "zip") {
-      var filename = getLocalNodeWebkitDownloadPath(os, arch, format);
       var unzipper = new DecompressZip(filename);
-      var targetPath = 'dist/' + os + arch;
 
       unzipper.on('error', function(err) {
         done(err);
@@ -118,24 +121,63 @@ module.exports = function(grunt) {
       unzipper.on('extract', function(log) {
         if(os == "osx") {
           grunt.file.expand(distFiles).forEach(function(file) {
-            grunt.file.copy(file, targetPath + "/node-webkit.app/Contents/Resources/app.nw/" + file);
+            if(grunt.file.isDir(file))
+              grunt.file.mkdir(file);
+            else
+              grunt.file.copy(file, targetPath + "/node-webkit.app/Contents/Resources/app.nw/" + file);
           });
           wrench.chmodSyncRecursive(targetPath, 0755);
+          done();
         } else if(os == "win") {
           grunt.file.expand(distFiles).forEach(function(file) {
-            grunt.file.copy(file, targetPath + "/" + file);
+            if(grunt.file.isDir(file))
+              grunt.file.mkdir(file);
+            else
+              grunt.file.copy(file, targetPath + "/" + file);
           });
+          done();
         }
       });
 
       unzipper.extract({
         path: targetPath
       });
+    } else if (format == 'tar.gz') {
+      fs.createReadStream(filename)
+        .pipe(zlib.createGunzip())
+        .pipe(tar.extract(targetPath))
+        .on('finish', function() {
+          var decompressedDir = targetPath + '/node-webkit-v' + nodeWebkitVersion + "-" + os + "-" + arch + "/";
+
+          fs.readdirSync(decompressedDir).forEach(function(file) {
+            grunt.file.copy(decompressedDir + file, targetPath + '/' + (file));
+          });
+
+          wrench.rmdirSyncRecursive(decompressedDir);
+          grunt.file.expand(distFiles).forEach(function(file) {
+            if(grunt.file.isDir(file))
+              grunt.file.mkdir(file);
+            else
+              grunt.file.copy(file, targetPath + "/" + file);
+          });
+          wrench.chmodSyncRecursive(targetPath, 0755);
+          done();
+        });
+
     }
-  })
+  });
+
+  grunt.registerTask('bundle-node-webkit-app-mac', ['download-node-webkit-mac', 'bundle-node-webkit-app:osx:ia32']);
+  grunt.registerTask('bundle-node-webkit-app-win', ['download-node-webkit-win','bundle-node-webkit-app:win:ia32']);
+  grunt.registerTask('bundle-node-webkit-app-linux32', ['download-node-webkit-linux32','bundle-node-webkit-app:linux:ia32']);
+  grunt.registerTask('bundle-node-webkit-app-linux64', ['download-node-webkit-linux64','bundle-node-webkit-app:linux:x64']);
 
 
   require('load-grunt-tasks')(grunt);
 
   grunt.registerTask('build-coffeescript', ['coffeelint', 'coffee:app']);
+  grunt.registerTask('build', ['build-coffeescript']);
+
+  grunt.registerTask('dist', ['build', 'bundle-node-webkit-app-mac', 'bundle-node-webkit-app-win',
+  'bundle-node-webkit-app-linux32', 'bundle-node-webkit-app-linux64'])
 }

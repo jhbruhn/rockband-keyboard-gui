685594512a5c846222c35022707864c10235904a
Jan-Henrik Bruhn <jh.bruhn@me.com>
2014-05-17 19:09:40 +0200

    Moved NW Distribution to its own plugin

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,91 +1,4 @@
-var wrench        = require('wrench'),
-    http          = require('http'),
-    url           = require('url'),
-    fs            = require('fs'),
-    ProgressBar   = require('progress'),
-    DecompressZip = require('decompress-zip'),
-    tar           = require('tar-fs'),
-    zlib          = require('zlib'),
-    archiver      = require('archiver'),
-    packageFile   = require('./package.json');
-
-var nodeWebkitVersion = "0.9.2";
-
-var nwFormats = {
-  "osx": "zip",
-  "win": "zip",
-  "linux": "tar.gz"
-}
-
-var distFiles = ["build/**/*.*", './node_modules/**', '!./node_modules/grunt*/**',
-             '!./node_modules/nodewebkit*/**', "package.json", 'index.html'];
-
-var downloadFile = function(targetFile, remoteUrl, done) {
-  var parsed = url.parse(remoteUrl);
-
-  var req = http.request({
-    host: parsed.hostname,
-    port: 80,
-    path: parsed.path
-  });
-
-  req.on('response', function(res){
-    var len = parseInt(res.headers['content-length'], 10);
-    res.pipe(fs.createWriteStream(targetFile));
-    console.log();
-    var bar = new ProgressBar('  downloading [:bar] :percent :etas', {
-      complete: '=',
-      incomplete: ' ',
-      width: 20,
-      total: len
-    });
-
-    res.on('data', function (chunk) {
-      bar.tick(chunk.length);
-    });
-
-    res.on('end', function () {
-      done();
-    });
-  });
-
-  req.end();
-}
-
-var zipFolder = function(folder, cwd, targetFile, cb) {
-  var output = fs.createWriteStream(targetFile);
-  var archive = archiver('zip');
-
-  output.on('close', function() {
-    cb();
-  });
-
-  archive.on('error', function(err) {
-    cb(err);
-  });
-
-  archive.pipe(output);
-  archive.bulk([
-      {
-        src: ["**/*", "!nwsnapshot*", "!credits.html"],
-        cwd: __dirname + "/" + folder,
-        expand: true
-      }
-    ]);
-
-  archive.finalize();
-
-}
-
-
-function getNodeWebkitDownloadURL(osName, archName, version, format) {
-  return "http://dl.node-webkit.org/v" + version + "/node-webkit-v" + version + "-" + osName + "-" + archName + "." + format;
-}
-
-function getLocalNodeWebkitDownloadPath(os, arch, format) {
-  return 'dist/cache' + "/node-webkit." + os + "." + arch + "." + nodeWebkitVersion + "." + format;
-}
-
+//
 module.exports = function(grunt) {
   require('time-grunt')(grunt);
 
@@ -109,108 +22,20 @@ module.exports = function(grunt) {
         files: ['coffee/**/*.iced'],
         tasks: ['build-coffeescript']
       }
-    }
-  });
+    },
 
-  grunt.registerTask('download-node-webkit', function(os, arch) {
-    var done = this.async();
-    var format = nwFormats[os];
-    var targetFolder = "dist/cache";
-    var targetFile = getLocalNodeWebkitDownloadPath(os, arch, format);
-    var remoteUrl = getNodeWebkitDownloadURL(os, arch, nodeWebkitVersion, format);
-
-    wrench.mkdirSyncRecursive(targetFolder);
-
-    if(fs.existsSync(targetFile)) {
-      grunt.log.writeln('skip download: file exists: ' + targetFile);
-      done();
-      return;
-    }
-
-    grunt.log.writeln('Starting download.');
-    downloadFile(targetFile, remoteUrl, done)
-  });
-
-  grunt.registerTask('download-node-webkit-mac', ['download-node-webkit:osx:ia32']);
-  grunt.registerTask('download-node-webkit-win', ['download-node-webkit:win:ia32']);
-  grunt.registerTask('download-node-webkit-linux32', ['download-node-webkit:linux:ia32']);
-  grunt.registerTask('download-node-webkit-linux64', ['download-node-webkit:linux:x64']);
-
-
-  grunt.registerTask('bundle-node-webkit-app', function(os, arch) {
-    var done = this.async();
-
-    var format = nwFormats[os];
-    var targetPath = 'dist/' + packageFile.name + "-" + os + "-" + arch;
-    var filename = getLocalNodeWebkitDownloadPath(os, arch, format);
-
-    if(format == "zip") {
-      var unzipper = new DecompressZip(filename);
-
-      unzipper.on('error', function(err) {
-        done(err);
-      });
-
-      unzipper.on('extract', function(log) {
-        if(os == "osx") {
-          grunt.file.expand(distFiles).forEach(function(file) {
-            if(grunt.file.isDir(file))
-              grunt.file.mkdir(file);
-            else
-              grunt.file.copy(file, targetPath + "/node-webkit.app/Contents/Resources/app.nw/" + file);
-
-          });
-          fs.renameSync(targetPath + "/node-webkit.app/", targetPath + "/Rockband Keyboard.app");
-
-          wrench.chmodSyncRecursive(targetPath, 0755);
-        } else if(os == "win") {
-          grunt.file.expand(distFiles).forEach(function(file) {
-            if(grunt.file.isDir(file))
-              grunt.file.mkdir(file);
-            else
-              grunt.file.copy(file, targetPath + "/" + file);
-          });
-        }
-        zipFolder(targetPath, "dist/", "dist/"  + packageFile.name + "-" + os + "-" + arch + ".zip", done);
-
-      });
-
-      unzipper.extract({
-        path: targetPath
-      });
-    } else if (format == 'tar.gz') {
-      fs.createReadStream(filename)
-        .pipe(zlib.createGunzip())
-        .pipe(tar.extract(targetPath))
-        .on('finish', function() {
-          var decompressedDir = targetPath + '/node-webkit-v' + nodeWebkitVersion + "-" + os + "-" + arch + "/";
-
-          fs.readdirSync(decompressedDir).forEach(function(file) {
-            grunt.file.copy(decompressedDir + file, targetPath + '/' + (file));
-          });
-
-          wrench.rmdirSyncRecursive(decompressedDir);
-          grunt.file.expand(distFiles).forEach(function(file) {
-            if(grunt.file.isDir(file))
-              grunt.file.mkdir(file);
-            else
-              grunt.file.copy(file, targetPath + "/" + file);
-          });
-          wrench.chmodSyncRecursive(targetPath, 0755);
-          zipFolder(targetPath, "dist/", "dist/" + packageFile.name + "-" + os + "-" + arch + ".zip", done);
-        });
+    build_node_webkit: {
+      targetDir: "dist",
+      nwVersion: "0.9.2",
+      distFiles: ["build/**/*.*", './node_modules/**', '!./node_modules/grunt*/**',
+                 '!./node_modules/nodewebkit*/**', "package.json", 'index.html'],
+      name: "rockband-keyboard-gui"
     }
   });
 
   grunt.registerTask('publish-package-json', function() {
     grunt.file.copy('./package.json', "dist/package.json");
-  })
-
-  grunt.registerTask('bundle-node-webkit-app-mac', ['download-node-webkit-mac', 'bundle-node-webkit-app:osx:ia32']);
-  grunt.registerTask('bundle-node-webkit-app-win', ['download-node-webkit-win','bundle-node-webkit-app:win:ia32']);
-  grunt.registerTask('bundle-node-webkit-app-linux32', ['download-node-webkit-linux32','bundle-node-webkit-app:linux:ia32']);
-  grunt.registerTask('bundle-node-webkit-app-linux64', ['download-node-webkit-linux64','bundle-node-webkit-app:linux:x64']);
-
+  });
 
   require('load-grunt-tasks')(grunt);
 

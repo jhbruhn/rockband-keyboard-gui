8eff303311bbf0a0b4a170078526baadcc496082
Jan-Henrik <jh.bruhn@me.com>
2014-05-18 21:46:15 +0200

    Added Travis device, first try

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -ev
+
+if [ ${TRAVIS_JOB_NUMBER##*.} -eq 1 ] && [ "$TRAVIS_BRANCH" = "master" ]; then
+  mkdir out/
+  cp dist/*.zip out/
+  cp dist/*.exe out/
+  cp dist/package.json out/
+  sudo pip install ghp-import
+	ghp-import -n dist/ -m "Deploy ${TRAVIS_BUILD_NUMBER}."
+	git push -fq https://$GIT_TOKEN@github.com/$TRAVIS_REPO_SLUG.git gh-pages > /dev/null
+fi
